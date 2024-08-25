68bb13329d0c1c609971059f6f58336b8aa21818
Michael Sperling <mesperli@usc.edu>
2024-08-24 20:07:39 -0700

    pass stage

--- app/main.py
+++ app/main.py
@@ -26,66 +26,58 @@ def main():
         line = 1
         while i < len(file_contents):
             c = file_contents[i]
-            if c == '(':
-                print("LEFT_PAREN ( null")
-            elif c == ')':
-                print("RIGHT_PAREN ) null")
-            elif c == '{':
-                print("LEFT_BRACE { null")
-            elif c == '}':
-                print("RIGHT_BRACE } null")
-            elif c == ',':
-                print("COMMA , null")
-            elif c == '.':
-                print("DOT . null")
-            elif c == '-':
-                print("MINUS - null")
-            elif c == '+':
-                print("PLUS + null")
-            elif c == ';':
-                print("SEMICOLON ; null")
-            elif c == '*':
-                print("STAR * null")
-            elif c == '!':
-                if i + 1 < len(file_contents) and file_contents[i+1] == '=':
-                    print("BANG_EQUAL != null")
-                    i += 1
-                else:
-                    print("BANG ! null")
-            elif c == '=':
-                if i + 1 < len(file_contents) and file_contents[i+1] == '=':
-                    print("EQUAL_EQUAL == null")
-                    i += 1
-                else:
-                    print("EQUAL = null")
-            elif c == '<':
-                if i + 1 < len(file_contents) and file_contents[i+1] == '=':
-                    print("LESS_EQUAL <= null")
-                    i += 1
-                else:
-                    print("LESS < null")
-            elif c == '>':
-                if i + 1 < len(file_contents) and file_contents[i+1] == '=':
-                    print("GREATER_EQUAL >= null")
-                    i += 1
-                else:
-                    print("GREATER > null")
-            elif c == '/':
-                if i + 1 < len(file_contents) and file_contents[i+1] == '/':
-                    i+=1
-                    while i < len(file_contents) and file_contents[i] != '\n':
+            match c:
+                case '(': print("LEFT_PAREN ( null")
+                case ')': print("RIGHT_PAREN ) null")
+                case '{': print("LEFT_BRACE { null")
+                case '}': print("RIGHT_BRACE } null")
+                case ',': print("COMMA , null")
+                case '.': print("DOT . null")
+                case '-': print("MINUS - null")
+                case '+': print("PLUS + null")
+                case ';': print("SEMICOLON ; null")
+                case '*': print("STAR * null")
+                case '!':
+                    if i + 1 < len(file_contents) and file_contents[i+1] == '=':
+                        print("BANG_EQUAL != null")
+                        i += 1
+                    else:
+                        print("BANG ! null")
+                case '=':
+                    if i + 1 < len(file_contents) and file_contents[i+1] == '=':
+                        print("EQUAL_EQUAL == null")
+                        i += 1
+                    else:
+                        print("EQUAL = null")
+                case '<':
+                    if i + 1 < len(file_contents) and file_contents[i+1] == '=':
+                        print("LESS_EQUAL <= null")
+                        i += 1
+                    else:
+                        print("LESS < null")
+                case '>':
+                    if i + 1 < len(file_contents) and file_contents[i+1] == '=':
+                        print("GREATER_EQUAL >= null")
+                        i += 1
+                    else:
+                        print("GREATER > null")
+                case '/':
+                    if i + 1 < len(file_contents) and file_contents[i+1] == '/':
                         i+=1
+                        while i < len(file_contents) and file_contents[i] != '\n':
+                            i+=1
+                        line += 1
+                    else:
+                        print("SLASH / null")
+                case ' ', '\t', '\r':
+                    pass
+                case '\n':
                     line += 1
-                else:
-                    print("SLASH / null")
-
-            else:
-                print(f"[line {line}] Error: Unexpected character: {c}", file=sys.stderr)
-                error = True
+                case _:
+                    print(f"[line {line}] Error: Unexpected character: {c}", file=sys.stderr)
+                    error = True
             i += 1
 
-                
-            
         print("EOF  null")
         if(error):
             exit(65)
