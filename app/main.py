eb29c9d07d45d1999a00399605192aa9b297b86e
Michael Sperling <mesperli@usc.edu>
2024-08-29 11:01:39 -0700

    pass stage

--- app/main.py
+++ app/main.py
@@ -86,6 +86,97 @@ def main():
                     else:
                         print(f"STRING {file_contents[i:j+1]} {file_contents[i+1:j]}")
                     i = j
+                # add cases for and, class, else, false, for, fun, if, nil, or, print, return, super, this, true, var, while.
+                case 'a':
+                    if i + 2 < len(file_contents) and file_contents[i:i+3] == 'and':
+                        print("AND and null")
+                        i += 2
+                    else:
+                        pass
+                case 'c':
+                    if i + 4 < len(file_contents) and file_contents[i:i+5] == 'class':
+                        print("CLASS class null")
+                        i += 4
+                    else:
+                        pass
+                case 'e':
+                    if i + 3 < len(file_contents) and file_contents[i:i+4] == 'else':
+                        print("ELSE else null")
+                        i += 3
+                    else:
+                        pass
+                case 'f':
+                    if i + 4 < len(file_contents) and file_contents[i:i+5] == 'false':
+                        print("FALSE false null")
+                        i += 4
+                    elif i + 2 < len(file_contents) and file_contents[i:i+3] == 'for':
+                        print("FOR for null")
+                        i += 2
+                    elif i + 2 < len(file_contents) and file_contents[i:i+3] == 'fun':
+                        print("FUN fun null")
+                        i += 2
+                    else:
+                        pass
+                case 'i':
+                    if i + 1 < len(file_contents) and file_contents[i:i+2] == 'if':
+                        print("IF if null")
+                        i += 1
+                    else:
+                        pass
+                case 'n':
+                    if i + 2 < len(file_contents) and file_contents[i:i+3] == 'nil':
+                        print("NIL nil null")
+                        i += 2
+                    else:
+                        pass
+                case 'o':
+                    if i + 1 < len(file_contents) and file_contents[i:i+2] == 'or':
+                        print("OR or null")
+                        i += 1
+                    else:
+                        pass
+                case 'p':
+                    if i + 4 < len(file_contents) and file_contents[i:i+5] == 'print':
+                        print("PRINT print null")
+                        i += 4
+                    else:
+                        pass
+                case 'r':
+                    if i + 5 < len(file_contents) and file_contents[i:i+6] == 'return':
+                        print("RETURN return null")
+                        i += 5
+                    else:
+                        pass
+                case 's':
+                    if i + 4 < len(file_contents) and file_contents[i:i+5] == 'super':
+                        print("SUPER super null")
+                        i += 4
+                    elif i + 3 < len(file_contents) and file_contents[i:i+4] == 'self':
+                        print("SELF self null")
+                        i += 3
+                    else:
+                        pass
+                case 't':
+                    if i + 3 < len(file_contents) and file_contents[i:i+4] == 'this':
+                        print("THIS this null")
+                        i += 3
+                    elif i + 3 < len(file_contents) and file_contents[i:i+4] == 'true':
+                        print("TRUE true null")
+                        i += 3
+                    else:
+                        pass
+                case 'v':
+                    if i + 2 < len(file_contents) and file_contents[i:i+3] == 'var':
+                        print("VAR var null")
+                        i += 2
+                    else:
+                        pass
+                case 'w':
+                    if i + 4 < len(file_contents) and file_contents[i:i+5] == 'while':
+                        print("WHILE while null")
+                        i += 4
+                    else:
+                        pass
                 case _ if c.isdigit():
                     decimal = False
                     j = i
